0d2f3f707fa98775f992ed0bfe2836b9c1462475
tagroup <thomas@theanellgroup.com>
2013-10-02 00:21:19 -0500

    Create sleep.php

---
+++ Client/sleep.php
@@ -0,0 +1,10 @@
+<?php
+/**
+*
+* Just a way to waste time and keep me from hitting f5.  With the thermostat and newer zwave the repolling time is longer.
+*/
+?>
+
+<meta http-equiv="Refresh" content="4; url=./">
+
+Updating your settings . . . 
