603b6dc82c26882ffe963faaa966359d0024bfd1
Willem Burgers <wa.burgers@gmail.com>
2013-11-03 20:59:11 +0100

    Fix the header file
    Forgot this one

--- Server/sunrise.h
+++ Server/sunrise.h
@@ -1,6 +1,6 @@
 #ifndef sunrise_h
 #define sunrise_h
 
-bool GetSunriseSunset(time_t &tSunrise,time_t &tSunset,time_t &tSunriseTomorrow,time_t &tSunsetTomorrow,float latitude,float longitude);
+bool GetSunriseSunset(time_t &tSunrise,time_t &tSunset,float latitude,float longitude);
 
 #endif
