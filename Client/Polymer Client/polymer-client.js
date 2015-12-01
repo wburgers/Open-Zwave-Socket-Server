3f897f1580c69694b572da3e6aab7239783ca813
Willem Burgers <wa.burgers@gmail.com>
2015-12-01 21:43:56 +0100

    Use websocket-component instead of JS websocket
    Use a Polymer wrapper of a websocket instead of all the Websocket code
    in JS.
    This cleans up the client code just a little bit

--- Client/Polymer Client/polymer-client.js
+++ Client/Polymer Client/polymer-client.js
@@ -1,5 +1,5 @@
 var app;
-var websocket;
+var websocketElement;
 var websocketStatus;
 var switchbutton;
 var google_signin;
@@ -9,6 +9,8 @@ var single_room;
 var scene_list;
 var sendinput;
 var drawerPanel;
+
+var commandSwitch;
 
 (function (document) {
 	'use strict';
@@ -23,7 +25,6 @@ var drawerPanel;
 	app.addEventListener('dom-change', function () {
 		registerGlobals();
 		registerEventListeners();
-		openWebsocket();
 	});
 
 	app.route = "rooms";
@@ -31,13 +32,21 @@ var drawerPanel;
 	// See https://github.com/Polymer/polymer/issues/1381
 	window.addEventListener('WebComponentsReady', function () {
 		document.querySelector('body').removeAttribute('unresolved');
+		
+		app.wsProtocols = 'open-zwave';
+		var connection = '';
+		if (secure) {
+			connection += 'wss://';
+		} else {
+			connection += 'ws://';
+		}
+		connection += server_ip + ':' + port;
+		app.wsURL = connection;
 	});
-
-	// Close drawer after menu item is selected if drawerPanel is narrow
-	app.onMenuSelect = function () {};
 })(document);
 
 function registerGlobals() {
+	websocketElement = document.querySelector('websocket-component');
 	websocketStatus = document.querySelector('#websocket-status');
 	switchbutton = document.querySelector('#switch-button');
 	google_signin = document.getElementById('google-signin');
@@ -48,24 +57,63 @@ function registerGlobals() {
 	scene_list = document.getElementById('scene-list');
 	sendinput = document.getElementById('sendinput');
 	drawerPanel = document.getElementById('paperDrawerPanel');
+	
+	commandSwitch = {
+		"AUTH" : function (parsed) {
+			if (parsed.auth === true) {
+				google_signin_status.profile = JSON.parse(parsed.profile);
+				google_signin_status.signedIn = true;
+				GetDeviceList();
+			}
+		},
+		"UPDATE" : function () {
+			GetDeviceList();
+		},
+		"ALIST" : function (parsed) {
+			single_room.Devices = parsed.nodes.slice();
+		},
+		"ROOMLIST" : function (parsed) {
+			room_list.Rooms = parsed.rooms.slice();
+			updateSingleRoom();
+		},
+		"ROOM" : function (parsed) {
+			room_list.Rooms.forEach(function (roomItem) {
+				if (roomItem.Name === parsed.room.Name) {
+					roomItem.currentTemp = parsed.room.currentTemp;
+					roomItem.currentSetpoint = parsed.room.currentSetpoint;
+					updateSingleRoom();
+
+				}
+			});
+		},
+		"SCENELIST" : function (parsed) {
+			scene_list.Scenes = parsed.scenes.slice();
+		},
+		"SWITCH" : function () {},
+		"ATHOME" : function (parsed) {
+			switchbutton.atHome = parsed.athome;
+		},
+		"default" : function () {},
+	};
 };
 
 function registerEventListeners() {
 	switchbutton.addEventListener('tap', function () {
 		switchbutton.changeColor();
-		websocket.send("SWITCH");
+		websocketElement.send("SWITCH");
 	});
 
 	sendinput.addEventListener('change', function () {
-		websocket.send(sendinput.value);
+		websocketElement.send(sendinput.value);
 		sendinput.value = "";
 	});
 
 	document.addEventListener('send-command', function (e) {
 		console.log("Sending command", e.detail.command);
-		websocket.send(e.detail.command);
+		websocketElement.send(e.detail.command);
 	});
 
+	// Close drawer after menu item is selected if drawerPanel is narrow
 	app.onMenuSelect = function () {
 		if (drawerPanel.narrow) {
 			drawerPanel.closeDrawer();
@@ -77,91 +125,36 @@ function registerEventListeners() {
 		single_room.roomName = e.detail.roomName;
 		updateSingleRoom();
 	});
+	
+	document.addEventListener('websocket-open', function() {
+		console.log("WEBSOCKET", "opening websocket");
+	});
+	
+	document.addEventListener('websocket-error', function(event) {
+		console.log("error: " + event.detail.data);
+	});
+	
+	document.addEventListener('websocket-close', function(event) {
+		console.log("Closed with code: " + event.detail.code + " " + event.detail.reason);
+	});
+	
+	document.addEventListener('websocket-message', function(event) {
+		console.log(event.detail.data);
+		var parsed = JSON.parse(event.detail.data);
+		var command = parsed.command;
+		if (commandSwitch[command]) {
+			commandSwitch[command](parsed);
+		} else {
+			commandSwitch["default"]();
+		}
+	});
 }
 
-function openWebsocket() {
-	var connection = '';
-	if (secure) {
-		connection += 'wss://';
-	} else {
-		connection += 'ws://';
-	}
-	connection += server_ip + ':' + port;
-	websocket = new WebSocket(connection, 'open-zwave');
-	try {
-		websocket.onopen = function () {
-			console.log("WEBSOCKET", "opening websocket");
-			websocketStatus.color = 'green';
-			websocketStatus.status = 'Connected';
-		};
-		websocket.onerror = function (error) {
-			websocketStatus.color = 'red';
-			websocketStatus.status = 'Error Check console';
-			console.log("error: " + error.data);
-		};
-		websocket.onmessage = function (message) {
-			console.log(message.data);
-			var parsed = JSON.parse(message.data);
-			var command = parsed.command;
-			var commandSwitch = {
-				"AUTH" : function () {
-					if (parsed.auth === true) {
-						google_signin_status.profile = JSON.parse(parsed.profile);
-						google_signin_status.signedIn = true;
-						GetDeviceList();
-					}
-				},
-				"UPDATE" : function () {
-					GetDeviceList();
-				},
-				"ALIST" : function () {
-					single_room.Devices = parsed.nodes.slice();
-				},
-				"ROOMLIST" : function () {
-					room_list.Rooms = parsed.rooms.slice();
-					updateSingleRoom();
-				},
-				"ROOM" : function () {
-					room_list.Rooms.forEach(function (roomItem) {
-						if (roomItem.Name === parsed.room.Name) {
-							roomItem.currentTemp = parsed.room.currentTemp;
-							roomItem.currentSetpoint = parsed.room.currentSetpoint;
-							updateSingleRoom();
-
-						}
-					});
-				},
-				"SCENELIST" : function () {
-					scene_list.Scenes = parsed.scenes.slice();
-				},
-				"SWITCH" : function () {},
-				"ATHOME" : function () {
-					switchbutton.atHome = parsed.athome;
-				},
-				"default" : function () {},
-			};
-
-			if (commandSwitch[command]) {
-				commandSwitch[command]();
-			} else {
-				commandSwitch["default"]();
-			}
-		};
-		websocket.onclose = function (event) {
-			websocketStatus.color = 'red';
-			websocketStatus.status = 'Closed, Check console';
-			console.log("Closed with code: " + event.code + " " + event.reason);
-		};
-	} catch (exception) {
-		alert('Error' + exception);
-	}
-};
-
 function GetDeviceList() {
-	websocket.send("ALIST");
-	websocket.send("ROOMLIST");
-	websocket.send("SCENELIST");
-	websocket.send("ATHOME");
+	websocketElement.send("ALIST");
+	websocketElement.send("ROOMLIST");
+	websocketElement.send("SCENELIST");
+	websocketElement.send("ATHOME");
 }
 
 function updateSingleRoom() {
